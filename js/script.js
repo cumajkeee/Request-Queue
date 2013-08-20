21d9f98d45509e5ec327e455761ed595d58afe4d
cumajkeee <serejkasimanovich@gmail.com>
2013-08-20 15:10:43 +0300

    Realy finished

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+function getXmlHttp(){
+    var xmlhttp;
+    try {
+        xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+        try {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (E) {
+            xmlhttp = false;
+        }
+    }
+    if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+        xmlhttp = new XMLHttpRequest();
+    }
+    return xmlhttp;
+}
+
 var queue = [];
 
 function load (url, type){
@@ -7,20 +24,30 @@ function load (url, type){
     }
 }
 
-function start (obj){
+function start (arr){
     var setLoader,
         i;
-    for (i = 0; i<obj.length; i++){
-        if (obj[i].type=="js"){
+    for (i = 0; i<arr.length; i++){
+        if (arr[i].type == "js"){
             setLoader = document.createElement('script');
-            setLoader.setAttribute("type","text/javascript");
-            setLoader.setAttribute("src", obj[i].url);
+            setLoader.setAttribute("src", arr[i].url);
         }
-        else if (obj[i].type=="css"){
+        else if (arr[i].type == "css"){
             setLoader = document.createElement("link");
             setLoader.setAttribute("rel", "stylesheet");
-            setLoader.setAttribute("type", "text/css");
-            setLoader.setAttribute("href", obj[i].url);
+            setLoader.setAttribute("href", arr[i].url);
+        }
+        else if (arr[i].type == "ajax"){
+            var xmlhttp = getXmlHttp();
+            xmlhttp.open('GET', arr[i].url, true);
+            xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState == 4) {
+                    if(xmlhttp.status == 200) {
+                        alert('success');
+                    }
+                }
+            };
+            xmlhttp.send(null);
         }
         if (typeof setLoader!="undefined"){
             document.getElementsByTagName("head")[0].appendChild(setLoader);
@@ -29,7 +56,8 @@ function start (obj){
     console.log(queue);
 }
 
-load('js/backbone-min.js', 'js');
-load('js/jquery.min.js', 'js');
 load('js/underscore-min.js', 'js');
+load('js/backbone-min.js', 'js');
+load('css/style.css', 'css');
+load('generated.json', 'ajax');
 start(queue);
