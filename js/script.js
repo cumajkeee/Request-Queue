aa8959c901129e9ed70a07b01d7af7c419d6ab91
cumajkeee <cumajkeee@gmail.com>
2013-09-09 12:11:57 +0300

    Added code: removed ActiveX, added onload log, support load arrays;

--- js/script.js
+++ js/script.js
@@ -1,45 +1,41 @@
 var RequestQueue = (function (){
     var queue = [];
     var request = {};
-
     function getXmlHttp(){
         var xmlhttp;
-        try {
-            xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
-        } catch (e) {
-            try {
-                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch (E) {
-                xmlhttp = false;
-            }
-        }
         if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
             xmlhttp = new XMLHttpRequest();
         }
         return xmlhttp;
     }
 
-    request.load = function (url, type){
-        queue[queue.length] = {
-            url : url,
-            type : type
+    request.load = function (elem){
+        if(Object.prototype.toString.call(elem) === '[object Array]' ) {
+            for (var i=0; i<elem.length; i++){
+                queue.push(elem[i]);
+            }
+        } else {
+            queue[queue.length] = elem;
         }
     };
 
     request.start = function (arr){
         var setLoader,
             i;
+        var status = 'loading';
         for (i = 0; i<arr.length; i++){
+            var log = arr[i].type+' file '+arr[i].url+' loaded successful';
             if (arr[i].type == "js"){
                 setLoader = document.createElement('script');
                 setLoader.setAttribute("src", arr[i].url);
-                console.log('JavaScript file '+arr[i].url+' loaded successful');
+                setLoader.addEventListener('load', console.log(log), false);
             }
             else if (arr[i].type == "css"){
                 setLoader = document.createElement("link");
                 setLoader.setAttribute("rel", "stylesheet");
                 setLoader.setAttribute("href", arr[i].url);
-                console.log('CSS file '+arr[i].url+' loaded successful');
+                setLoader.addEventListener('load', console.log(log), false);
+
             }
             if (typeof setLoader!="undefined"){
                 document.getElementsByTagName("head")[0].appendChild(setLoader);
@@ -51,7 +47,7 @@ var RequestQueue = (function (){
                 xmlhttp.onreadystatechange = function() {
                     if (xmlhttp.readyState == 4) {
                         if(xmlhttp.status == 200) {
-                            console.log('AJAX request '+arr[i].url+' completed');
+                            console.log('AJAX request completed');
                         }
                     }
                 };
@@ -64,11 +60,8 @@ var RequestQueue = (function (){
         return request;
     };
 
-    request.load('js/underscore-min.js', 'js');
-    request.load('js/backbone-min.js', 'js');
-    request.load('css/style.css', 'css');
-    request.load('generated.json', 'ajax');
+    request.load({url:'js/underscore-min.js',type:'js'});
+    request.load([{url:'js/backbone-min.js', type:'js'},{url:'css/style.css', type:'css'}]);
+    request.load({url:'generated.json', type:'ajax'});
     request.start(queue);
 })();
-
-
